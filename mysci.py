2b9c22488ccb493885ee325f33a81f4d9688da93
Julia Kent <jkent@ucar.edu>
2022-06-09 13:24:08 -0600

    Adding script file

--- mysci.py
+++ mysci.py
@@ -1,3 +1,5 @@
-# Printing hello world
-print("Hello, world!")
-print("vim")
+# Read the data file
+filename = "data/wxobs20170821.txt"
+
+with open(filename, 'r') as datafile:
+    data = datafile.read()
