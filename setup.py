4f36d1de9107976b4ed29d4ec84b472871cb079d
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-09-01 13:16:47 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package description",
+    author="Julia Kent",
+    author_email="jkent@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
